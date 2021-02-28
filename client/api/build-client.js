90695342ff96a99bf2a7f6f6f9d8c1aba66a8251
krearthur <artgrohe@googlemail.com>
2021-02-28 10:53:24 +0100

    changed host back to agrohe-dev

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -10,7 +10,7 @@ const buildClient = ({ req }) => {
 
     return axios.create({
       // baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      baseURL: 'https://dev.krearthur.de',
+      baseURL: 'https://www.agrohe-dev-ticketing.xyz',
       headers: req.headers
     });
   }
